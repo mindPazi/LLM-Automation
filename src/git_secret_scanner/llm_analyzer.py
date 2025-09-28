a97812eccb582f6215cc6f60494d3d0471ed6f94
mindPazi <gitmind87@gmail.com>
2025-09-28 10:17:11 +0200

    Updated requirements and LLMAnalyzer structure

--- src/git_secret_scanner/llm_analyzer.py
+++ src/git_secret_scanner/llm_analyzer.py
@@ -1,7 +1,15 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
 class LLMAnalyzer:
     
-    def __init__(self, model_name="gpt-3.5-turbo"):
+    def __init__(self, model_name="llama"):
         self.model_name = model_name
+        self.model = None
+        self.tokenizer = None
+    
+    def load_model(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_name)
     
     def analyze_diff(self, diff_content):
         pass
